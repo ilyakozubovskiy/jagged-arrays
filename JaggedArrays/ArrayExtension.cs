e2e982fdf9d797360d7e72693d4acc37779d4310
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-10-22 11:36:37 +0000

    Upload New File

---
+++ JaggedArrays/ArrayExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JaggedArrays
+{
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// Sorts the rows in a two-dimensional jagged by increase of the sum of the elements in them.
+        /// </summary>
+        /// <param name="source">The two-dimensional jagged array to sort.</param>
+        /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
+        public static void Sort(this int[][] source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
