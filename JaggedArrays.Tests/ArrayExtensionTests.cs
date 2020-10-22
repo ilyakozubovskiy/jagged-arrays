0d120ed5c49dd688093aa5ce7dcb80f59030b899
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-10-22 17:14:20 +0300

    add-new-methods

--- JaggedArrays.Tests/ArrayExtensionTests.cs
+++ JaggedArrays.Tests/ArrayExtensionTests.cs
@@ -9,38 +9,7 @@ namespace JaggedArrays.Tests
     [TestFixture]
     public class ArrayExtensionTests
     {
-        public static IEnumerable<TestCaseData> TestCases
-        {
-            get
-            {
-                yield return new TestCaseData(
-                    new[] { new[] { 2, -40, 55 }, new[] { 1 }, new[] { 1, 1 }, null, null },
-                    new[] { null, null, new[] { 1 }, new[] { 1, 1 }, new[] { 2, -40, 55 } });
-                yield return new TestCaseData(
-                    new[] { new[] { 1, 2, 3 }, null, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1 } },
-                    new[] { null, new[] { 1 }, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 3 }, new[] { 1, 2, 1, 3 } });
-                yield return new TestCaseData(
-                    new[] { new[] { 1, 2, 3 }, null, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1 }, new[] { 10, 10, 1, 90, 1 } },
-                    new[] { null, new[] { 1 }, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 3 }, new[] { 1, 2, 1, 3 }, new[] { 10, 10, 1, 90, 1 } });
-                yield return new TestCaseData(
-                    new[] { new[] { 1, 2, 3 }, new[] { 10987, 2, 3 }, null, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1 }, new[] { 10, 10, 1, 90, 1 }, null },
-                    new[] { null, null, new[] { 1 }, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 3 }, new[] { 1, 2, 1, 3 }, new[] { 10, 10, 1, 90, 1 }, new[] { 10987, 2, 3 } });
-                yield return new TestCaseData(
-                    new[] { new[] { 2, -40, 55 }, new[] { 1 }, new[] { 1, 1 }, null, null },
-                    new[] { new[] { 2, -40, 55 }, new[] { 1, 1 }, new[] { 1 }, null, null });
-                yield return new TestCaseData(
-                    new[] { new[] { 1, 2, 3 }, null, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1 } },
-                    new[] { new[] { 1, 2, 1, 3 }, new[] { 1, 2, 3 }, new[] { 1, 1, 1, 1, 1 }, new[] { 1 }, null });
-                yield return new TestCaseData(
-                    new[] { new[] { 1, 2, 3 }, null, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1 }, new[] { 10, 10, 1, 90, 1 } },
-                    new[] { new[] { 10, 10, 1, 90, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1, 2, 3 }, new[] { 1, 1, 1, 1, 1 }, new[] { 1 }, null });
-                yield return new TestCaseData(
-                    new[] { new[] { 1, 2, 3 }, new[] { 10987, 2, 3 }, null, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1 }, new[] { 10, 10, 1, 90, 1 }, null },
-                    new[] { new[] { 10987, 2, 3 }, new[] { 10, 10, 1, 90, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1, 2, 3 }, new[] { 1, 1, 1, 1, 1 }, new[] { 1 }, null, null });
-            }
-        }
-
-        [TestCaseSource(nameof(TestCases))]
+        [TestCaseSource(typeof(TestCasesDataSource), nameof(TestCasesDataSource.TestCasesForSumByAscending))]
         public void OrderByAscendingBySum_Tests(int[][] source, int[][] expected)
         {
             source.OrderByAscendingBySum();
@@ -50,8 +19,8 @@ namespace JaggedArrays.Tests
         [Test]
         public void OrderByAscendingBySum_SourceIsNull_ThrowArgumentNullException() =>
             Assert.Throws<ArgumentNullException>(() => ArrayExtension.OrderByAscendingBySum(null));
-        
-        [TestCaseSource(nameof(TestCases))]
+
+        [TestCaseSource(typeof(TestCasesDataSource), nameof(TestCasesDataSource.TestCasesForSumByDescending))]
         public void OrderByDescendingBySum_Tests(int[][] source, int[][] expected)
         {
             source.OrderByDescendingBySum();
@@ -61,5 +30,27 @@ namespace JaggedArrays.Tests
         [Test]
         public void OrderByDescendingBySum_SourceIsNull_ThrowArgumentNullException() =>
             Assert.Throws<ArgumentNullException>(() => ArrayExtension.OrderByDescendingBySum(null));
+
+        [TestCaseSource(typeof(TestCasesDataSource), nameof(TestCasesDataSource.TestCasesForMaxAscending))]
+        public void OrderByAscendingByMax_Tests(int[][] source, int[][] expected)
+        {
+            source.OrderByAscendingByMax();
+            Assert.AreEqual(expected, source);
+        }
+
+        [Test]
+        public void OrderByAscendingByMax_SourceIsNull_ThrowArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() => ArrayExtension.OrderByAscendingByMax(null));
+
+        [TestCaseSource(typeof(TestCasesDataSource), nameof(TestCasesDataSource.TestCasesForMaxDescending))]
+        public void OrderByDescendingByMax_Tests(int[][] source, int[][] expected)
+        {
+            source.OrderByDescendingByMax();
+            Assert.AreEqual(expected, source);
+        }
+
+        [Test]
+        public void OrderByDescendingByMax_SourceIsNull_ThrowArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() => ArrayExtension.OrderByDescendingByMax(null));
     }
 }
--- JaggedArrays/ArrayExtension.cs
+++ JaggedArrays/ArrayExtension.cs
@@ -23,5 +23,25 @@ namespace JaggedArrays
         {
             throw new NotImplementedException();
         }
+        
+        /// <summary>
+        /// Orders the rows in a jagged-array by ascending of the max of the elements in them.
+        /// </summary>
+        /// <param name="source">The jagged-array to sort.</param>
+        /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
+        public static void OrderByAscendingByMax(this int[][] source)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Orders the rows in a jagged-array by descending of the max of the elements in them.
+        /// </summary>
+        /// <param name="source">The jagged-array to sort.</param>
+        /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
+        public static void OrderByDescendingByMax(this int[][] source)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
