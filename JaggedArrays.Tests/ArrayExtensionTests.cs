6b920ac874fb65a7064bbf05943810dbdf392c34
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-10-22 16:47:55 +0300

    add-new-methods

--- JaggedArrays.Tests/ArrayExtensionTests.cs
+++ JaggedArrays.Tests/ArrayExtensionTests.cs
@@ -25,18 +25,41 @@ namespace JaggedArrays.Tests
                 yield return new TestCaseData(
                     new[] { new[] { 1, 2, 3 }, new[] { 10987, 2, 3 }, null, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1 }, new[] { 10, 10, 1, 90, 1 }, null },
                     new[] { null, null, new[] { 1 }, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 3 }, new[] { 1, 2, 1, 3 }, new[] { 10, 10, 1, 90, 1 }, new[] { 10987, 2, 3 } });
+                yield return new TestCaseData(
+                    new[] { new[] { 2, -40, 55 }, new[] { 1 }, new[] { 1, 1 }, null, null },
+                    new[] { new[] { 2, -40, 55 }, new[] { 1, 1 }, new[] { 1 }, null, null });
+                yield return new TestCaseData(
+                    new[] { new[] { 1, 2, 3 }, null, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1 } },
+                    new[] { new[] { 1, 2, 1, 3 }, new[] { 1, 2, 3 }, new[] { 1, 1, 1, 1, 1 }, new[] { 1 }, null });
+                yield return new TestCaseData(
+                    new[] { new[] { 1, 2, 3 }, null, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1 }, new[] { 10, 10, 1, 90, 1 } },
+                    new[] { new[] { 10, 10, 1, 90, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1, 2, 3 }, new[] { 1, 1, 1, 1, 1 }, new[] { 1 }, null });
+                yield return new TestCaseData(
+                    new[] { new[] { 1, 2, 3 }, new[] { 10987, 2, 3 }, null, new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1 }, new[] { 10, 10, 1, 90, 1 }, null },
+                    new[] { new[] { 10987, 2, 3 }, new[] { 10, 10, 1, 90, 1 }, new[] { 1, 2, 1, 3 }, new[] { 1, 2, 3 }, new[] { 1, 1, 1, 1, 1 }, new[] { 1 }, null, null });
             }
         }
 
         [TestCaseSource(nameof(TestCases))]
-        public void Sort_Tests(int[][] source, int[][] expected)
+        public void OrderByAscendingBySum_Tests(int[][] source, int[][] expected)
         {
-            source.Sort();
+            source.OrderByAscendingBySum();
             Assert.AreEqual(expected, source);
         }
 
         [Test]
-        public void Sort_SourceIsNull_ThrowArgumentNullException() =>
-            Assert.Throws<ArgumentNullException>(() => ArrayExtension.Sort(null));
+        public void OrderByAscendingBySum_SourceIsNull_ThrowArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() => ArrayExtension.OrderByAscendingBySum(null));
+        
+        [TestCaseSource(nameof(TestCases))]
+        public void OrderByDescendingBySum_Tests(int[][] source, int[][] expected)
+        {
+            source.OrderByDescendingBySum();
+            Assert.AreEqual(expected, source);
+        }
+
+        [Test]
+        public void OrderByDescendingBySum_SourceIsNull_ThrowArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() => ArrayExtension.OrderByDescendingBySum(null));
     }
 }
--- JaggedArrays/ArrayExtension.cs
+++ JaggedArrays/ArrayExtension.cs
@@ -5,11 +5,21 @@ namespace JaggedArrays
     public static class ArrayExtension
     {
         /// <summary>
-        /// Sorts the rows in a two-dimensional jagged by increase of the sum of the elements in them.
+        /// Orders the rows in a jagged-array by ascending of the sum of the elements in them.
         /// </summary>
-        /// <param name="source">The two-dimensional jagged array to sort.</param>
+        /// <param name="source">The jagged-array to sort.</param>
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
-        public static void Sort(this int[][] source)
+        public static void OrderByAscendingBySum(this int[][] source)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Orders the rows in a jagged-array by descending of the sum of the elements in them.
+        /// </summary>
+        /// <param name="source">The jagged-array to sort.</param>
+        /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
+        public static void OrderByDescendingBySum(this int[][] source)
         {
             throw new NotImplementedException();
         }
